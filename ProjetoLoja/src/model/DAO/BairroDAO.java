6d8ee1e4b05c4a0dc40b584e90ea82d42319218b
Gustavo Cancelier <Gustavo Cancelier>
2021-12-03 19:47:32 -0300

    criar factory connection

--- ProjetoLoja/src/model/DAO/BairroDAO.java
+++ ProjetoLoja/src/model/DAO/BairroDAO.java
@@ -8,37 +8,32 @@ public class BairroDAO implements InterfaceDAO<Bairro>{
 
 	@Override
 	public void create(Bairro objeto) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public List<Bairro> retrieve() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public Bairro retrieve(int codigo) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Bairro retrieve(String descricao) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void update(Bairro objeto) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void delete(Bairro objeto) {
-		// TODO Auto-generated method stub
 		
 	}
     
--- ProjetoLoja/src/model/DAO/ConnectionFactory.java
+++ ProjetoLoja/src/model/DAO/ConnectionFactory.java
@@ -1,5 +1,15 @@
 package model.DAO;
 
+import java.sql.Connection;
+
 public class ConnectionFactory {
-    
+	
+	private static final String driver = "com.mysql.jdbc.Driver";
+	private static final String banco = "jdbc.mysql://localhost:3306/bancoloja";
+	private static final String user = "root";
+	private static final String senha = "ifsc";
+	
+    public static Connection getConnection() {
+    	return null;
+    }
 }
